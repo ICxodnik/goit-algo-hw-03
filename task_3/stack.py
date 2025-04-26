fc35dbe85f380d389a2ffb9cda337330767acaa6
Natalia <icxodnik@gmail.com>
2025-04-26 12:37:25 +0200

    add stack implementation

---
+++ task_3/stack.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Stack:
+    def __init__(self, *args):
+        self._container = deque(args)
+
+    def push(self, value):
+        if not self.empty() and value > self.peek():
+            raise Exception("Stack error")  
+        self._container.append(value)
+
+    def pop(self):
+        return self._container.pop()
+
+    def peek(self):
+        if not self.empty():
+            return self._container[-1]
+        return None
+
+    def empty(self):
+        return len(self._container) == 0
+    
+    def __len__(self):
+        return len(self._container)
+
+    def __repr__(self):
+        return repr(list(self._container))
